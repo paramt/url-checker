df6b06ed9adf3538974e506f661b5ef0897b7c89
Param Thakkar <pht0104@gmail.com>
2019-10-02 17:22:44 -0400

    Check if blacklisted exists in env variables

--- check_links.py
+++ check_links.py
@@ -6,7 +6,9 @@ from urlextract import URLExtract
 import requests
 
 # URLs to skip over
-blacklisted = os.getenv('BLACKLISTED').split(",")
+blacklisted = ""
+if "blacklisted" in os.environ:
+    blacklisted = os.getenv('BLACKLISTED').split(",")
 
 files = os.getenv('FILES').split(",")
 links = []
